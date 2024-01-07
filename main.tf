d09e329359b1ec98b0e75119623bc5df081fde2a
nitukumarii <vatsnitu12@gmail.com>
2024-01-07 18:24:35 +0530

    version error updated

--- main.tf
+++ main.tf
@@ -1,32 +1,36 @@
-data "aws_ami" "ubuntu" {
-    most_recent = true
-filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
-    }
-filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-owners = ["099720109477"] # Canonical
-}
-
 terraform {
   required_providers {
     aws = "~> 3.0.0"
-    terraform = ">=v1.6.6"
+    terraform = ">= v1.6.6"
   }
 }
 
 provider "aws" {
-  region  = "us-east-2"
+  region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name      = "app-ssh-key"
-tags = {
+
+  tags = {
     Name = var.myinstance
   }
 }
